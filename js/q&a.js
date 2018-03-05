21d5645d328238f041a574727cc9d5478e6c178b
José Marques <zezu.marques@gmail.com>
2018-03-05 16:08:14 +0000

    Fixed Navbar js

--- js/q&a.js
+++ js/q&a.js
@@ -13,10 +13,13 @@ $(".navbar-toggler").click(pushBreadcrombsdown);
 function pushBreadcrombsdown() {
   if ($("#navbarSupportedContent").hasClass("show")) {
     $("#breadcrumbs").css("margin-top", "55px");
-    $("body").css("padding-top", "110px");
+    $("#myPage").css("padding-top", "110px");
+    $("#index").css("padding-top", "55px");
+
   } else {
     $("#breadcrumbs").css("margin-top", "206px");
-    $("body").css("padding-top", "316px");
+    $("#myPage").css("padding-top", "316px");
+    $("#index").css("padding-top", "206px");
     $(".dropdown-toggle").click(pushBreadcrombsdown2);
   }
 }
@@ -24,7 +27,11 @@ function pushBreadcrombsdown() {
 function pushBreadcrombsdown2() {
   if ($(".dropdown").hasClass("show")) {
     $("#breadcrumbs").css("margin-top", "206px");
+    $("#index").css("padding-top", "206px");
+    $("#myPage").css("padding-top", "316px");
   } else {
     $("#breadcrumbs").css("margin-top", "351px");
+    $("#index").css("padding-top", "351px");
+    $("#myPage").css("padding-top", "402px");
   }
 }
