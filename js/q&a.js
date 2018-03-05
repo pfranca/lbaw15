3faef4c6e39101e92fa10b0a5d3aff51f4695a1c
José Marques <zezu.marques@gmail.com>
2018-03-05 14:28:58 +0000

    Theme Page fixed navbar

--- js/q&a.js
+++ js/q&a.js
@@ -13,8 +13,10 @@ $(".navbar-toggler").click(pushBreadcrombsdown);
 function pushBreadcrombsdown() {
   if ($("#navbarSupportedContent").hasClass("show")) {
     $("#breadcrumbs").css("margin-top", "55px");
+    $("body").css("padding-top", "110px");
   } else {
     $("#breadcrumbs").css("margin-top", "206px");
+    $("body").css("padding-top", "316px");
     $(".dropdown-toggle").click(pushBreadcrombsdown2);
   }
 }
