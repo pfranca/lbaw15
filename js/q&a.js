f54104f756c69894d368316a99889fb9538ddca0
José Marques <zezu.marques@gmail.com>
2018-03-05 13:29:45 +0000

    Initial Edit
    Bug Dropdown behind second nav

--- js/q&a.js
+++ js/q&a.js
@@ -6,3 +6,23 @@ function goToTopic() {
     scrollTop: $("#themes").offset().top
   }, 1000);
 }
+
+$(".navbar-toggler").click(pushBreadcrombsdown);
+
+
+function pushBreadcrombsdown() {
+  if ($("#navbarSupportedContent").hasClass("show")) {
+    $("#breadcrumbs").css("margin-top", "55px");
+  } else {
+    $("#breadcrumbs").css("margin-top", "206px");
+    $(".dropdown-toggle").click(pushBreadcrombsdown2);
+  }
+}
+
+function pushBreadcrombsdown2() {
+  if ($(".dropdown").hasClass("show")) {
+    $("#breadcrumbs").css("margin-top", "206px");
+  } else {
+    $("#breadcrumbs").css("margin-top", "351px");
+  }
+}
