f0daf4ddfc86ea7bf4ed7ec23bf0d931b8ca086d
pfranca <pedro.franca.1994@gmail.com>
2018-03-24 17:20:50 +0000

    update database

--- database/qna.sql
+++ database/qna.sql
@@ -1,238 +1,125 @@
-CREATE TABLE answer (
-    id integer NOT NULL,
-    date timestamp without time zone DEFAULT now() NOT NULL,
-    karma integer DEFAULT 0 NOT NULL,
-    message text NOT NULL,
-    id_user integer,
-    id_question integer,
-    disable BOOLEAN NOT NULL DEFAULT FALSE
+DROP TABLE IF EXISTS topicTag CASCADE;
+DROP TABLE IF EXISTS questionTag CASCADE;
+DROP TABLE IF EXISTS tag CASCADE;
+DROP TABLE IF EXISTS notificationFollow CASCADE;
+DROP TABLE IF EXISTS notificationAnswer CASCADE;
+DROP TABLE IF EXISTS vote CASCADE;
+DROP TABLE IF EXISTS followQuestion CASCADE;
+DROP TABLE IF EXISTS followTopic CASCADE;
+DROP TABLE IF EXISTS reportAnswer CASCADE;
+DROP TABLE IF EXISTS reportQuestion CASCADE;
+DROP TABLE IF EXISTS answer CASCADE;
+DROP TABLE IF EXISTS question CASCADE;
+DROP TABLE IF EXISTS topic CASCADE;
+DROP TABLE IF EXISTS "user" CASCADE;
+
+CREATE TYPE user_type AS ENUM ('NORMAL', 'MOD', 'ADMIN');
+
+CREATE TABLE "user"(
+  id SERIAL UNIQUE,
+  username VARCHAR(30) NOT NULL UNIQUE,
+  email TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL,
+  img TEXT NOT NULL,
+  bio TEXT,
+  disable BOOLEAN DEFAULT FALSE NOT NULL,
+  type user_type NOT NULL
 );
 
-
-CREATE TABLE followquestion (
-    id_user integer NOT NULL,
-    id_question integer NOT NULL
+CREATE TABLE topic(
+  id SERIAL UNIQUE,
+  name VARCHAR(30) NOT NULL UNIQUE,
+  img TEXT NOT NULL,
+  disable BOOLEAN DEFAULT FALSE NOT NULL
 );
 
-
-CREATE TABLE followtopic (
-    id_user integer NOT NULL,
-    id_topic integer NOT NULL
+CREATE TABLE question(
+  id SERIAL UNIQUE,
+  "date" TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  karma INTEGER DEFAULT 0 NOT NULL,
+  short_message TEXT NOT NULL,
+  long_message TEXT ,
+  id_author INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+  id_topic INTEGER NOT NULL REFERENCES topic(id) ON UPDATE CASCADE,
+  disable BOOLEAN DEFAULT FALSE NOT NULL
 );
 
+CREATE TABLE answer(
+  id SERIAL UNIQUE,
+  "date" TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL,
+  karma INTEGER DEFAULT 0 NOT NULL,
+  message TEXT NOT NULL,
+  id_author INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+  id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE,
+  disable BOOLEAN DEFAULT FALSE NOT NULL
+);
 
+CREATE TABLE reportQuestion(
+  id SERIAL UNIQUE,
+  reason TEXT NOT NULL,
+  id_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+  id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE
+);
 
-CREATE TABLE notification (
-    id integer NOT NULL,
-    id_user integer NOT NULL,
-    id_question integer
+CREATE TABLE reportAnswer(
+  id SERIAL UNIQUE,
+  reason TEXT NOT NULL,
+  id_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+  id_answer INTEGER NOT NULL REFERENCES answer(id) ON UPDATE CASCADE
+);
+
+CREATE TABLE followTopic(
+  id_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+  id_topic INTEGER NOT NULL REFERENCES topic(id) ON UPDATE CASCADE,
+  PRIMARY KEY(id_user, id_topic)
+);
+
+CREATE TABLE followQuestion(
+ id_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+ id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE,
+  PRIMARY KEY(id_user, id_question)
+);
+
+CREATE TABLE vote(
+    id SERIAL UNIQUE,
+    id_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+    vote BOOLEAN NOT NULL,
+    id_answer INTEGER NOT NULL REFERENCES answer(id) ON UPDATE CASCADE,
+    id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE,
+    CONSTRAINT "only one" CHECK ((((id_answer IS NULL) AND (id_question IS NOT NULL)) OR ((id_answer IS NOT NULL) AND (id_question IS NULL))))
+);
+
+CREATE TABLE notificationAnswer(
+    id SERIAL UNIQUE,
+    notificated_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+    id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE,
+    message TEXT  NOT NULL,
+    seen BOOLEAN DEFAULT FALSE NOT NULL, 
+    "date" TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
+);
+
+CREATE TABLE notificationFollow(
+    id SERIAL UNIQUE,
+  	notificated_user INTEGER NOT NULL REFERENCES "user"(id) ON UPDATE CASCADE,
+    id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE,
     message TEXT NOT NULL,
-    seen BOOLEAN NOT NULL DEFAULT FALSE, 
-    date TIMESTAMP DEAFULT now() NOT NULL
-
+    seen BOOLEAN DEFAULT FALSE NOT NULL, 
+    "date" TIMESTAMP WITH TIME ZONE DEFAULT now() NOT NULL
 );
 
-
-
-CREATE TABLE question (
-    id integer NOT NULL,
-    date timestamp with time zone DEFAULT now() NOT NULL,
-    karma integer DEFAULT 0 NOT NULL,
-    short_message text NOT NULL,
-    long_message text,
-    id_user integer,
-    id_topic integer,
-    disable BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE tag(
+    id SERIAL UNIQUE,
+    tagName VARCHAR(20) NOT NULL
 );
 
-
-CREATE TABLE report (
-    id integer NOT NULL,
-    reason text NOT NULL,
-    id_user integer,
-    id_answer integer,
-    id_question integer,
-    id_notification integer,
-    CONSTRAINT only_one CHECK ((((id_answer IS NULL) AND (id_question IS NOT NULL)) OR ((id_answer IS NOT NULL) AND (id_question IS NULL))))
+CREATE TABLE questionTag(
+  id_question INTEGER NOT NULL REFERENCES question(id) ON UPDATE CASCADE,
+  id_tag INTEGER NOT NULL REFERENCES tag(id) ON UPDATE CASCADE,
+  PRIMARY KEY(id_question, id_tag)
 );
 
-
-
-CREATE TABLE topic (
-    id integer NOT NULL,
-    name character varying(30) NOT NULL,
-    img text NOT NULL,
-    disable BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE topicTag(
+  id_topic INTEGER NOT NULL REFERENCES topic(id) ON UPDATE CASCADE,
+ id_tag INTEGER NOT NULL REFERENCES tag(id) ON UPDATE CASCADE,
+  PRIMARY KEY(id_topic, id_tag)
 );
-
-
-
-CREATE TABLE "user" (
-    id integer NOT NULL,
-    username character varying(30) NOT NULL,
-    email text NOT NULL,
-    name text NOT NULL,
-    img text NOT NULL,
-    bio text,
-    disable BOOLEAN NOT NULL DEFAULT FALSE,
-    type text NOT NULL,
-    CONSTRAINT type CHECK ((type = ANY (ARRAY['Normal'::text, 'Moderator'::text, 'Administrator'::text])))
-);
-
-
-CREATE TABLE vote (
-    id integer NOT NULL,
-    id_user integer,
-    vote integer NOT NULL,
-    id_answer integer,
-    id_question integer,
-    CONSTRAINT "only one" CHECK ((((id_answer IS NULL) AND (id_question IS NOT NULL)) OR ((id_answer IS NOT NULL) AND (id_question IS NULL)))),
-    CONSTRAINT vote_vote_check CHECK (((vote >= (-1)) AND (vote <= 1)))
-);
-
-
-ALTER TABLE vote OWNER TO lbaw1715;
-
-
-ALTER TABLE vote_id_seq OWNER TO lbaw1715;
-
-
-ALTER TABLE ONLY answer ALTER COLUMN id SET DEFAULT nextval('answer_id_seq'::regclass);
-
-
-ALTER TABLE ONLY notification ALTER COLUMN id SET DEFAULT nextval('notification_id_seq'::regclass);
-
-
-ALTER TABLE ONLY question ALTER COLUMN id SET DEFAULT nextval('question_id_seq'::regclass);
-
-
-ALTER TABLE ONLY report ALTER COLUMN id SET DEFAULT nextval('report_id_seq'::regclass);
-
-
-ALTER TABLE ONLY topic ALTER COLUMN id SET DEFAULT nextval('topic_id_seq'::regclass);
-
-
-ALTER TABLE ONLY "user" ALTER COLUMN id SET DEFAULT nextval('user_id_seq'::regclass);
-
-
-ALTER TABLE ONLY vote ALTER COLUMN id SET DEFAULT nextval('vote_id_seq'::regclass);
-
-
-
-ALTER TABLE ONLY answer
-    ADD CONSTRAINT answer_pkey PRIMARY KEY (id);
-
-
-
-ALTER TABLE ONLY followquestion
-    ADD CONSTRAINT followquestion_pkey PRIMARY KEY (id_user, id_question);
-
-
-
-ALTER TABLE ONLY followtopic
-    ADD CONSTRAINT followtopic_pkey PRIMARY KEY (id_user, id_topic);
-
-
-ALTER TABLE ONLY notification
-    ADD CONSTRAINT notification_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY question
-    ADD CONSTRAINT question_pkey PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY report
-    ADD CONSTRAINT report_pkey PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY topic
-    ADD CONSTRAINT topic_name_key UNIQUE (name);
-
-
-ALTER TABLE ONLY topic
-    ADD CONSTRAINT topic_pkey PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_email_key UNIQUE (email);
-
-
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_pkey PRIMARY KEY (id);
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_username_key UNIQUE (username);
-
-
-ALTER TABLE ONLY vote
-    ADD CONSTRAINT vote_pkey PRIMARY KEY (id);
-
-
-ALTER TABLE ONLY answer
-    ADD CONSTRAINT answer_id_question_fkey FOREIGN KEY (id_question) REFERENCES question(id);
-
-ALTER TABLE ONLY answer
-    ADD CONSTRAINT answer_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
-
-ALTER TABLE ONLY followquestion
-    ADD CONSTRAINT followquestion_id_question_fkey FOREIGN KEY (id_question) REFERENCES question(id);
-
-
-
-ALTER TABLE ONLY followquestion
-    ADD CONSTRAINT followquestion_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
-
-ALTER TABLE ONLY followtopic
-    ADD CONSTRAINT followtopic_id_topic_fkey FOREIGN KEY (id_topic) REFERENCES topic(id);
-
-
-ALTER TABLE ONLY followtopic
-    ADD CONSTRAINT followtopic_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
-
-ALTER TABLE ONLY notification
-    ADD CONSTRAINT notification_id_question_fkey FOREIGN KEY (id_question) REFERENCES question(id);
-
-
-ALTER TABLE ONLY notification
-    ADD CONSTRAINT notification_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
-
-ALTER TABLE ONLY question
-    ADD CONSTRAINT question_id_topic_fkey FOREIGN KEY (id_topic) REFERENCES topic(id);
-
-
-
-ALTER TABLE ONLY question
-    ADD CONSTRAINT question_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
-
-ALTER TABLE ONLY report
-    ADD CONSTRAINT report_id_answer_fkey FOREIGN KEY (id_answer) REFERENCES answer(id);
-
-
-ALTER TABLE ONLY report
-    ADD CONSTRAINT report_id_notification_fkey FOREIGN KEY (id_notification) REFERENCES notification(id);
-
-
-ALTER TABLE ONLY report
-    ADD CONSTRAINT report_id_question_fkey FOREIGN KEY (id_question) REFERENCES question(id);
-
-ALTER TABLE ONLY report
-    ADD CONSTRAINT report_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
-
-
-ALTER TABLE ONLY vote
-    ADD CONSTRAINT vote_id_answer_fkey FOREIGN KEY (id_answer) REFERENCES answer(id);
-
-
-ALTER TABLE ONLY vote
-    ADD CONSTRAINT vote_id_question_fkey FOREIGN KEY (id_question) REFERENCES question(id);
-
-
-ALTER TABLE ONLY vote
-    ADD CONSTRAINT vote_id_user_fkey FOREIGN KEY (id_user) REFERENCES "user"(id);
-
