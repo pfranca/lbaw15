e76ccb81d26dc2134793d113436fcf9eafebceba
MartaTorgal <up201407727@fe.up.pt>
2018-05-28 10:21:23 +0100

    ajax action follow button

--- resources/views/partials/question.blade.php
+++ resources/views/partials/question.blade.php
@@ -33,17 +33,17 @@
             <a href="#" data-id="{{$question->id}}" data-toggle="modal" data-target="#reportModal" data-dismiss="modal" class="underTab colorLink">Report</a>
             <a href="#" data-id="{{$question->id}}" data-toggle="modal" data-target="#questionDelModal" data-dismiss="modal" class="underTab colorLink" id="deleteQuestion">Delete </a>
             @if(Auth::user()->followQuestionId($question->id,Auth::user()->id))
-              <button id="followAnswer" onclick="actionFolloQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%"> Unfollow </button>
+              <button id="followQuestion" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%"> Unfollow </button>
             @else
-              <button id="unfollowAnswer" onclick="actionFolloQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%"> Follow </button>
+              <button id="followQuestion" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%"> Follow </button>
             @endif  
           @else
           <a href="{{$topic->name}}/question/{{$question->id}}" class="underTab colorLink">Answer</a>
           <a href="" data-id="{{$question->id}}" data-toggle="modal" data-target="#reportModal" data-dismiss="modal" class="underTab colorLink">Report</a>
           @if(Auth::user()->followQuestionId($question->id,Auth::user()->id))
-              <button id="followAnswer" onclick="actionFolloQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%;border-radius: 20px; background-color: #4da6ff; color: white; border-style: none;padding-right: 20px; padding-left: 20px"> Unfollow </button>
+              <button id="followQuestion" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%;border-radius: 20px; background-color: #4da6ff; color: white; border-style: none;padding-right: 20px; padding-left: 20px"> Unfollow </button>
             @else
-              <button id="unfollowAnswer" onclick="actionFolloQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%;border-radius: 20px; background-color: #4da6ff; color: white; border-style: none; font-size: 2vmin; padding-right: 20px; padding-left: 20px"> Follow </button>
+              <button id="followQuestion" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%;border-radius: 20px; background-color: #4da6ff; color: white; border-style: none; font-size: 2vmin; padding-right: 20px; padding-left: 20px"> Follow </button>
             @endif  
           @endif
         @endguest
