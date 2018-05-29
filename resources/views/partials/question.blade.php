6561acd378cbf0977f7fbb0214526892176061e7
Diogo Almeida Cunha <diogocunha588@gmail.com>
2018-05-29 13:08:14 +0100

    collors of follow and unfollow corrected

--- resources/views/partials/question.blade.php
+++ resources/views/partials/question.blade.php
@@ -52,9 +52,9 @@
       <div class="row text-right text-bottom col-md-6 pull-right" >
         <div class="col-md-12 mg-b-5">
           @if(Auth::user()->followQuestionId($question->id,Auth::user()->id))
-              <button id="followQuestion{{$question->id}}" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%;border-radius: 20px; background-color: #004466; color: white; border-style: none; font-size: 1.8vmin; padding-right: 20px; padding-left: 20px"> Unfollow </button>
+              <button id="followQuestion{{$question->id}}" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion Unfollow-btn"> Unfollow </button>
             @else
-              <button id="followQuestion{{$question->id}}" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion" style="margin-left: 2%;border-radius: 20px; background-color: #4da6ff; color: white; border-style: none; font-size: 2vmin; padding-right: 20px; padding-left: 20px"> Follow </button>
+              <button id="followQuestion{{$question->id}}" onclick="actionFollowQuestion('{{$question->id}}')" type="button" class="buttonDown followCardQuestion follow-btn"> Follow </button>
             @endif   
         </div>
         <div class="col-md-12">
