df398e45ccd95514e90269192c91fdafc871521b
Diogo Almeida Cunha <diogocunha588@gmail.com>
2018-05-29 12:19:11 +0100

    profile layout done

--- resources/views/partials/profileSidebar.blade.php
+++ resources/views/partials/profileSidebar.blade.php
@@ -4,18 +4,18 @@
       <div class="col-md-12 pl-3 nav1" style="">
 
         <div class="title-nav"></div>
-          <div class="col-md-12 d-flex justify-content-center" style="margin-top: -70px;">
-            <img class="img-profile" src="{{asset('images/'.$user->img)}}">
+          <div class="col-md-12 d-flex justify-content-center setTop">
+            <img class=" setImage img-profile " src="{{asset('images/'.$user->img)}}">
           </div>
           <div class="userSide d-flex justify-content-center " >
             <ul class="fa-ul">
-              <li><i class="fa-li fas fa-envelope"></i>{{$user->email}}</li>
-              <li><i class="fa-li fa fa-spinner fa-thumbs-up"></i>Karma :  332</li>
-              <li><i class="fa-li fas fa-keyboard"></i>Answers : 28</li>
+              <li ><i class="fa-li fas fa-envelope icon-top"></i>{{$user->email}}</li>
+              <li><i class="fa-li fa fa-spinner fa-thumbs-up icon-top"></i>Karma :  332</li>
+              <li><i class="fa-li fas fa-keyboard icon-top"></i>Answers : 28</li>
             </ul>
           </div>
 
-        <div class="title-nav" style="border-top-style:2px solid black;">Sorted</div>
+        <div class="title-nav sorted">Sorted</div>
         <div class="text-left pr-1 mt-1 nav-sort">        
           <select>
             <option value="Our special sauce" selected>
